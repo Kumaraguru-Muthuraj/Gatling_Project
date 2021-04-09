511a105af1540646f3f02d05659724ddacd20261
Kumaraguru-Muthuraj <kumaraguru1976@yahoo.com>
2021-04-09 17:42:36 +0530

    Load test e-commerce website

--- demostore-gatling-demo/src/gatling/scala/gatlingdemostore/DemostoreSimulation.scala
+++ demostore-gatling-demo/src/gatling/scala/gatlingdemostore/DemostoreSimulation.scala
@@ -8,63 +8,48 @@ import io.gatling.jdbc.Predef._
 
 class DemostoreSimulation extends Simulation {
 
+  val domain = "demostore.gatling.io"
+
 	val httpProtocol = http
-		.baseUrl("http://demostore.gatling.io")
-		.inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.woff2""", """.*\.(t|o)tf""", """.*\.png""", """.*detectportal\.firefox\.com.*"""), WhiteList())
-		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-		.acceptEncodingHeader("gzip, deflate")
-		.acceptLanguageHeader("en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7")
-		.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36")
+		.baseUrl("http://" + domain)
 
-	val headers_0 = Map("Upgrade-Insecure-Requests" -> "1")
+    // Tiding up the project
+    //.inferHtmlResources(BlackList(""".*\.js""", """.*\.css""", """.*\.gif""", """.*\.jpeg""", """.*\.jpg""", """.*\.ico""", """.*\.woff""", """.*\.woff2""", """.*\.(t|o)tf""", """.*\.png""", """.*detectportal\.firefox\.com.*"""), WhiteList())
+		//.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+		//.acceptEncodingHeader("gzip, deflate")
+		//.acceptLanguageHeader("en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7")
+		//.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36")
 
-	val headers_4 = Map(
-		"Accept" -> "*/*",
-		"X-Requested-With" -> "XMLHttpRequest")
-
-	val headers_6 = Map(
-		"Cache-Control" -> "max-age=0",
-		"Origin" -> "http://demostore.gatling.io",
-		"Upgrade-Insecure-Requests" -> "1")
-
-
-
+  //Removed headers for tidying up
 	val scn = scenario("DemostoreSimulation")
-		.exec(http("request_0")
+		.exec(http("Load Home Page")
 			.get("/")
-      .check(css("#_csrf", "content").saveAs("csrfValue"))
-			.headers(headers_0))
-		.pause(9)
-		.exec(http("request_1")
-			.get("/about-us")
-			.headers(headers_0))
-		.pause(9)
-		.exec(http("request_2")
-			.get("/category/all")
-			.headers(headers_0))
-		.pause(10)
-		.exec(http("request_3")
-			.get("/product/black-and-red-glasses")
-			.headers(headers_0))
-		.pause(11)
-		.exec(http("request_4")
-			.get("/cart/add/19")
-			.headers(headers_4))
-		.pause(9)
-		.exec(http("request_5")
-			.get("/cart/view")
-			.headers(headers_0))
-		.pause(16)
-		.exec(http("request_6")
+      .check(regex("<title>Gatling Demo-Store</title>").exists)
+      .check(css("#_csrf", "content").saveAs("csrfValue")))
+		.pause(2)
+		.exec(http("Load About Us Page")
+			.get("/about-us"))
+    .pause(2)
+		.exec(http("Load Categories Page")
+			.get("/category/all"))
+		.pause(2)
+		.exec(http("Load Product Page")
+			.get("/product/black-and-red-glasses"))
+		.pause(2)
+		.exec(http("Add Product to Cart")
+			.get("/cart/add/19"))
+		.pause(2)
+		.exec(http("View Cart")
+			.get("/cart/view"))
+		.pause(2)
+		.exec(http("Login User")
 			.post("/login")
-			.headers(headers_6)
 			.formParam("_csrf", "${csrfValue}")
 			.formParam("username", "user1")
 			.formParam("password", "pass"))
-		.pause(16)
-		.exec(http("request_7")
-			.get("/cart/checkout")
-			.headers(headers_0))
+		.pause(2)
+		.exec(http("Checkout")
+			.get("/cart/checkout"))
 
 	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 }
