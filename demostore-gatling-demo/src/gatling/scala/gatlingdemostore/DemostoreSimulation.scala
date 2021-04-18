a34c460c65c08d90126936c579e812d3821a8026
Kumaraguru-Muthuraj <kumaraguru1976@yahoo.com>
2021-04-18 21:20:19 +0530

    Added concurrent test documentation

--- demostore-gatling-demo/src/gatling/scala/gatlingdemostore/DemostoreSimulation.scala
+++ demostore-gatling-demo/src/gatling/scala/gatlingdemostore/DemostoreSimulation.scala
@@ -22,8 +22,10 @@ class DemostoreSimulation extends Simulation {
   }
 
   def userCount: Int = getProperty("USERS", "5").toInt
+  //Ramp duration - N users ramped up per rampDuration of unit of time.
   def rampDuration: Int = getProperty("RAMP_DURATION", "10")toInt
-  def testDuration: Int = getProperty("DURATION", "60").toInt
+  //Total time when the test will run. rampDuration <<< testDuration
+  def testDuration: Int = getProperty("DURATION", "120").toInt
 
   def getProperty(propertyName: String, defaultValue: String): String = {
     Option(System.getenv(propertyName))
@@ -81,6 +83,12 @@ class DemostoreSimulation extends Simulation {
   object UserJourneys {
     def minPause = 100.milliseconds
     def maxPause = 500 milliseconds
+
+    def pingHomePage = {
+      exec(initSession)
+        .exec(CmsPages.homePage)
+        .pause(2)
+    }
 
     def browseStore = {
       exec(initSession)
@@ -140,12 +148,60 @@ class DemostoreSimulation extends Simulation {
           50d -> exec(UserJourneys.completePurchase)
         )
       }
+    def highBrowsing  = scenario ("High Browsing Load Test")
+      .during(/*testDuration*/300) {
+          exec(UserJourneys.pingHomePage)
+      }
   }
 
+  /*
+  Load testing with concurrent users for a sustained period of time.
+  Consider the following load test code
+  def highBrowsing  = scenario ("High Browsing Load Test")
+      .during(300) {
+          exec(UserJourneys.pingHomePage)
+      }
+      .inject(
+      constantConcurrentUsers(10).during(20.seconds),
+      rampConcurrentUsers(10).to(50).during(100.seconds)
+    ).protocols(httpProtocol)
+
+    Interpretation:
+    1) Start with creating 10 concurrent users in 20 seconds
+    2) Ramp up the concurrent users from 10 to 50 in 100 seconds
+    3) After reaching 100 seconds and 50 concurrent users, sustain that 50 concurrent users
+       for about 300 seconds. So from the point of start of test (1) from 0th second, till 300 seconds the test will run
+       and then the number of concurrent users will drop as requests are completed after 300th second.
+    4) So from the 0th to 20th second, we have 10 concurrent users.
+       From 20th to 120th second (100 seconds) the number of users are ramped up to 50.
+       From 120th second to 300th second, the number of concurrent users are fixed at 50.
+       After 300th second, no new user is spawned and as the requests are fulfilled, the users are killed.
+    5) You will see a pattern like this. 
+              ,----------------------------
+             /                             \
+            /                               \___
+           /                                    \
+          /                                      \
+         /                                        \
+     ___/                                          \
+                                                    \
+                                                     \
+    6) Note: If the pause times between requests are not sufficient enough, you get a 429 from the Gateway.
+
+   */
+
   //RUN THE default and high purchase scenarios in PARALLEL
-  setUp(
+  /*setUp(
     Scenarios.default.inject(rampUsers(userCount) during (rampDuration seconds)).protocols(httpProtocol),
     Scenarios.highPurchase.inject(rampUsers(5) during (60 seconds)).protocols(httpProtocol)
+  )*/
+
+  //CONCURRENT USERS - TEST
+  setUp(
+    Scenarios.highBrowsing.inject(
+      constantConcurrentUsers(10).during(20.seconds),
+      rampConcurrentUsers(10).to(50).during(100.seconds)
+    ).protocols(httpProtocol)
   )
 
   // RUN THE default and high purchase scenarios in SEQUENCE
